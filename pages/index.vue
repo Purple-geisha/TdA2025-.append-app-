603d62899716732c0d7bb912ce16603fc3073f15
Purple-geisha <daniel2kral@gmail.com>
2025-01-05 23:01:23 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -9,39 +9,5 @@
 }
 </style>
 <template>
-    <p :class="$style.purple">Hello TdA</p>
+    <div :class="$style.purple">Hello TdA</div>
 </template>
-
-//.circle{
-    position: middle;
-    width: 80px;
-    height: 80px;
-    border-radius:50%;
-}
-.circle::before,
-.circle:after{
-    content:"";
-    position: absolute;
-    border-radius: inherit;
-}
-.circle:before{
-        width: 100%;
-        height: 100%;
-        background-image:linear-gradient(
-            0deg,#ff00cc 0%,
-            #323297 100%);
-        animation: spin .5s infinite linear;
-}
-.circle:after{
-    width: 85%;
-    height: 85%;
-    background-color: rgb(1, 8, 17);
-    top:50%;
-    left:50%;
-    transform: translate(-50%, -50%);
-}
-@keyframes spin {
-    to {
-        transform: rotate(360deg);
-    }
-}
