d406ad74f6804b236d496e897ef2cf2bc1d3e210
Purple-geisha <daniel2kral@gmail.com>
2025-01-05 23:16:57 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,3 @@
-
-<style module>
-.purple{
-    font-style: italic;
-    font-family: sans-serif;
-    font-size: 100px;
-    color:#d6970e;
-    border: 4px dotted rgba(0, 0, 0, 0.315);
-}
-</style>
 <template>
-    <p :class="$style.purple">Hello TdA</p>
+    <div>Hello TdA</div>
 </template>
